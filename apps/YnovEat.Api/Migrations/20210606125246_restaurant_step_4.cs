88a248fd7d701662871f5c4188d5ad55ebd079fb
Rael Calitro <rael06@hotmail.fr>
2021-06-06 14:58:03 +0200

    restaurant db step 4

---
+++ apps/YnovEat.Api/Migrations/20210606125246_restaurant_step_4.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace YnovEat.Api.Migrations
+{
+    public partial class restaurant_step_4 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "RestaurantCategoryLinks",
+                columns: table => new
+                {
+                    RestaurantId = table.Column<int>(type: "int", nullable: false),
+                    CategoryId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RestaurantCategoryLinks", x => new { x.RestaurantId, x.CategoryId });
+                    table.ForeignKey(
+                        name: "FK_RestaurantCategoryLinks_Categories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Categories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_RestaurantCategoryLinks_Restaurants_RestaurantId",
+                        column: x => x.RestaurantId,
+                        principalTable: "Restaurants",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RestaurantCategoryLinks_CategoryId",
+                table: "RestaurantCategoryLinks",
+                column: "CategoryId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "RestaurantCategoryLinks");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+        }
+    }
+}
---
+++ src/YnovEat.Domain/ModelsAggregate/RestaurantAggregate/Category.cs
@@ -0,0 +1,8 @@
+namespace YnovEat.Domain.ModelsAggregate.RestaurantAggregate
+{
+    public class RestaurantCategory
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
