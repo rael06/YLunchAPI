d1c9fe701aa94633057a0ae387c1c5c7b207cbe2
Rael Calitro <rael06@hotmail.fr>
2021-10-26 12:36:19 +0200

    change min temperature to default

--- YLunch.API/Controllers/WeatherForecastController.cs
+++ YLunch.API/Controllers/WeatherForecastController.cs
@@ -36,7 +36,7 @@ namespace YLunch.API.Controllers
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
                 {
                     Date = DateTime.Now.AddDays(index),
-                    TemperatureC = rng.Next(-20000, 55),
+                    TemperatureC = rng.Next(-20, 55),
                     Summary = Summaries[rng.Next(Summaries.Length)]
                 })
                 .ToArray();
