1fe559519e64f052279f471463a950baf92013bf
Rael Calitro <rael06@hotmail.fr>
2021-11-02 17:21:28 +0100

    update .gitignore

---
+++ tests/YLunch.Application.Tests/ContextBuilder.cs
@@ -0,0 +1,19 @@
+using System;
+using Microsoft.EntityFrameworkCore;
+using YLunch.Infrastructure.Database;
+
+namespace YLunch.Application.Tests
+{
+    public static class ContextBuilder
+    {
+        public static ApplicationDbContext BuildContext()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: $"YLunchDatabase-{Guid.NewGuid()}")
+                .Options;
+
+            // Insert seed data into the database using one instance of the context
+            return new ApplicationDbContext(options);
+        }
+    }
+}
