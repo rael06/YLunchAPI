f9aff2ae118b5eb9ff9ab2368f91ff3a307aa57e
Rael Calitro <rael06@hotmail.fr>
2021-11-03 00:55:46 +0100

    add library fluent assertions for tests

--- src/YLunch.Domain/DTO/UserModels/UserAsCustomerDetailsReadDto.cs
+++ src/YLunch.Domain/DTO/UserModels/UserAsCustomerDetailsReadDto.cs
@@ -3,7 +3,7 @@ using YLunch.Domain.ModelsAggregate.UserAggregate;
 
 namespace YLunch.Domain.DTO.UserModels
 {
-    public class UserAsCustomerDetailsReadDto
+    public sealed class UserAsCustomerDetailsReadDto
     {
         public string Id { get; set; }
         public string Firstname { get; set; }
@@ -18,25 +18,6 @@ namespace YLunch.Domain.DTO.UserModels
             Lastname = entity.Lastname;
             Email = entity.NormalizedEmail;
             PhoneNumber = entity.PhoneNumber;
-        }
-
-        public bool Equals(UserAsCustomerDetailsReadDto other)
-        {
-            return Id == other.Id && Firstname == other.Firstname && Lastname == other.Lastname &&
-                   Email == other.Email && PhoneNumber == other.PhoneNumber;
-        }
-
-        public override bool Equals(object obj)
-        {
-            if (ReferenceEquals(null, obj)) return false;
-            if (ReferenceEquals(this, obj)) return true;
-            if (obj.GetType() != this.GetType()) return false;
-            return Equals((UserAsCustomerDetailsReadDto) obj);
-        }
-
-        public override int GetHashCode()
-        {
-            return HashCode.Combine(Id, Firstname, Lastname, Email, PhoneNumber);
         }
     }
 }
