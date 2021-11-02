b79eac51be19fe62fae67382dfd8308eb1487f74
Rael Calitro <rael06@hotmail.fr>
2021-11-02 16:39:01 +0100

    address minor issues

--- apps/YLunch.Api/Controllers/ApiController.cs
+++ apps/YLunch.Api/Controllers/ApiController.cs
@@ -11,6 +11,11 @@ namespace YLunch.Api.Controllers
     [Route("api")]
     public class ApiController : CustomControllerBase
     {
+        private const string API_RUNNING_MESSAGE = "Api is running";
+
+        private static readonly string API_RUNNING_AS_AUTHENTICATED_MESSAGE =
+            $"{API_RUNNING_MESSAGE}, and you're authenticated";
+
         public ApiController(
             UserManager<User> userManager,
             IUserRepository userRepository,
@@ -21,33 +26,31 @@ namespace YLunch.Api.Controllers
 
         [HttpGet("try")]
         [AllowAnonymous]
-        public string Try() =>
-            "Api is running";
+        public string Try() => API_RUNNING_MESSAGE;
 
         [HttpGet("try-authenticated")]
         [Core.Authorize]
-        public string TryAsAuthenticated() =>
-            "Api is running, and you're authenticated";
+        public string TryAsAuthenticated() => API_RUNNING_AS_AUTHENTICATED_MESSAGE;
 
         [HttpGet("try-authenticated-superAdmin")]
         [Core.Authorize(UserRoles.SuperAdmin)]
         public string TryAsAuthenticatedSuperAdmin() =>
-            "Api is running, and you're a " + UserRoles.SuperAdmin;
+            $"{API_RUNNING_MESSAGE}, and you're a {UserRoles.SuperAdmin}";
 
 
         [HttpGet("try-authenticated-restaurantAdmin")]
         [Core.Authorize(UserRoles.RestaurantAdmin)]
         public string TryAsAuthenticatedRestaurantAdmin() =>
-            "Api is running, and you're a " + UserRoles.RestaurantAdmin;
+            $"{API_RUNNING_MESSAGE}, and you're a {UserRoles.RestaurantAdmin}";
 
         [HttpGet("try-authenticated-employee")]
         [Core.Authorize(UserRoles.Employee)]
         public string TryAsAuthenticatedEmployee() =>
-            "Api is running, and you're a " + UserRoles.Employee;
+            $"{API_RUNNING_MESSAGE}, and you're a {UserRoles.Employee}";
 
         [HttpGet("try-authenticated-customer")]
         [Core.Authorize(UserRoles.Customer)]
         public string TryAsAuthenticatedCustomer() =>
-            "Api is running, and you're a " + UserRoles.Customer;
+            $"{API_RUNNING_MESSAGE}, and you're a {UserRoles.Customer}";
     }
 }
