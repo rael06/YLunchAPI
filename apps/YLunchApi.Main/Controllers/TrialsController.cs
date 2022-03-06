a02b8da1a9316e8881c9ed25895fdffcb1cf93ee
rael06 <47027233+rael06@users.noreply.github.com>
2022-03-06 22:03:25 +0100

    improve-message-dto (#48)

--- apps/YLunchApi.Main/Controllers/TrialsController.cs
+++ apps/YLunchApi.Main/Controllers/TrialsController.cs
@@ -14,14 +14,14 @@ public class TrialsController : ApplicationControllerBase
     }
 
     [HttpGet("anonymous")]
-    public ActionResult<string> GetAnonymousTry()
+    public ActionResult<MessageDto> GetAnonymousTry()
     {
         return Ok(new MessageDto("YLunchApi is running, you are anonymous."));
     }
 
     [HttpGet("authenticated")]
     [Authorize]
-    public ActionResult<string> GetAuthenticatedTry()
+    public ActionResult<MessageDto> GetAuthenticatedTry()
     {
         return Ok(new MessageDto(
             $"YLunchApi is running, you are authenticated as {CurrentUserEmail} with Id: {CurrentUserId} and Roles: {Roles.ListToString(CurrentUserRoles)}."));
@@ -29,7 +29,7 @@ public class TrialsController : ApplicationControllerBase
 
     [HttpGet("authenticated-customer")]
     [Authorize(Roles = Roles.Customer)]
-    public ActionResult<string> GetAuthenticatedCustomerTry()
+    public ActionResult<MessageDto> GetAuthenticatedCustomerTry()
     {
         return Ok(new MessageDto(
             $"YLunchApi is running, you are authenticated as {CurrentUserEmail} with Id: {CurrentUserId} and Roles: {Roles.ListToString(CurrentUserRoles)}."));
@@ -37,7 +37,7 @@ public class TrialsController : ApplicationControllerBase
 
     [HttpGet("authenticated-restaurant-admin")]
     [Authorize(Roles = Roles.RestaurantAdmin)]
-    public ActionResult<string> GetAuthenticatedRestaurantAdminTry()
+    public ActionResult<MessageDto> GetAuthenticatedRestaurantAdminTry()
     {
         return Ok(new MessageDto(
             $"YLunchApi is running, you are authenticated as {CurrentUserEmail} with Id: {CurrentUserId} and Roles: {Roles.ListToString(CurrentUserRoles)}."));
