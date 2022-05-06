99aada8798e7d04f6f9859f053a2faad845ea1a2
Rael Calitro <rael06@hotmail.fr>
2022-05-06 17:11:57 +0200

    rename state to orderState in db orderstatuses

--- apps/YLunchApi.Main/Migrations/20220506151050_Rename_State_To_OrderState_In_OrderStatuses.Designer.cs
+++ apps/YLunchApi.Main/Migrations/20220506151050_Rename_State_To_OrderState_In_OrderStatuses.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using YLunchApi.Infrastructure.Database;
 
@@ -10,9 +11,10 @@ using YLunchApi.Infrastructure.Database;
 namespace YLunchApi.Main.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220506151050_Rename_State_To_OrderState_In_OrderStatuses")]
+    partial class Rename_State_To_OrderState_In_OrderStatuses
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ apps/YLunchApi.Main/Migrations/20220506151050_Rename_State_To_OrderState_In_OrderStatuses.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace YLunchApi.Main.Migrations
+{
+    public partial class Rename_State_To_OrderState_In_OrderStatuses : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "State",
+                table: "OrderStatuses",
+                newName: "OrderState");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "OrderState",
+                table: "OrderStatuses",
+                newName: "State");
+        }
+    }
+}
